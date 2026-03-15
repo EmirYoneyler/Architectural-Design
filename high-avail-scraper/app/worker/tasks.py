403151cd2b042a6f23479d8d2375152545c86507
emir yoneyler <emiryoneyler@gmail.com>
2026-03-15 17:18:40 +0300

    lolololo

--- high-avail-scraper/app/worker/tasks.py
+++ high-avail-scraper/app/worker/tasks.py
@@ -1,20 +1,27 @@
-from celery import Celery
-import time
-import random
-import os
-
-redis_url = os.getenv("REDIS_URL", "redis://localhost:6373/0")
-app = Celery('tasks', broker=redis_url, backend=redis_url)
-
-@app.task(bind=True, max_retries=3, queue="scraper")
+import httpx
+from .tasks import app
+from app.core.identity import identity_manager
+@app.task(bind=True,max_retries=5)
 def scrape_target_site(self,url):
-    try:
-        print(f"[*]{url} - Scraping started.")
-        if random.random() < 0.3:  # Simulate a failure with 30% chance
-            raise Exception("Simulated scraping failure.")
-        time.sleep(2)
-        return {"url": url, "status": "success", "data": f"Scraped data from {url}"}  # Simulate variable scraping time
-    except Exception as exc:
-        print(f"[*]{url} - Scraping failed: {exc}")
-        raise self.retry(exc=exc, countdown=2 ** self.request.retries)
+    identity = identity_manager.get_identity()
+    headers = {"User-Agent": identity.user_agent}
+    proxy=identity["proxy"]
     
+    try:
+        with httpx.Client(proxies=proxy, headers=headers, timeout=10) as client:
+            response = client.get(url)
+            if response.status_code in [403, 429]:
+                raise httpx.HTTPStatusError(f"we got blocked with status code {response.status_code}", request=response.request, response=response)
+            
+            
+            return {
+                "url": url,
+                "status_code": response.status_code,
+                "data_length": len(response.content),
+                "used_proxy": proxy,
+            }
+        #         # Process the response as needed
+    except Exception as exc:
+        wait_time = 2 ** self.request.retries
+        print(f"Error scraping {url}: {exc}. Retrying in {wait_time} seconds...")
+        raise self.retry(exc=exc, countdown=wait_time)
